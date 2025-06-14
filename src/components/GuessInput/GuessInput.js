de7ae8184d51c5f01a12feccd7254cf043243d6d
Rashid Enahora <rashidenahora@Rashids-MacBook-Pro.local>
2025-06-14 15:35:11 -0400

    Completed the GuessResults Component

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,36 +1,35 @@
 import React from 'react';
 
-function GuessInput() {
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
 
-  const [guess, setGuess] = React.useState('');
-  
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log({guess});
-    setGuess('');
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleSubmitGuess(tentativeGuess);
+
+    setTentativeGuess('');
   }
 
-  return <form 
-            className="guess-input-wrapper"
-            onSubmit={handleSubmit}
-          >
-            <label htmlFor="guess-input">Enter guess:</label>
-            <input 
-              required
-              minLength={5}
-              maxLength={5}
-              pattern="[a-zA-Z]{5}"
-              title="Only enter a 5 letter word please 😎"
-              type="text" 
-              id="guess-input"
-              value={guess}
-              onChange={(e) => {
-                const upperWord = e.target.value.toUpperCase();
-                setGuess(upperWord);
-              }}              
-            />
-        </form>;
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+      />
+    </form>
+  );
 }
-
 
 export default GuessInput;
