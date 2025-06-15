6936f93e3b48d753502c50401fc33a7a4985852c
Rashid Enahora <rashidenahora@Rashids-MacBook-Pro.local>
2025-06-15 16:17:26 -0400

    Added some logic to GuessResults component

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Guess from '../Guess';   
-import {range} from '../../utils';
-import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
 
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
 
-function GuessResults({ guesses }) {
+import Guess from '../Guess';
+
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-          <Guess key={num} value={guesses[num]} />
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   );
